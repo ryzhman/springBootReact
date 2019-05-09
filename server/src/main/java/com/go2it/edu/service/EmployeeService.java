b772f27f7e566bc329c16768d58cc11168826d0b
Oleksandr Ryzhkov <oryzhkov@paymentus.com>
2019-05-09 00:00:37 -0400

    Server side is separate

--- server/src/main/java/com/go2it/edu/service/EmployeeService.java
+++ server/src/main/java/com/go2it/edu/service/EmployeeService.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -15,6 +14,8 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.springframework.stereotype.Service;
+
+import org.apache.http.HttpResponse;
 
 /**
  * @author Alex Ryzhkov
